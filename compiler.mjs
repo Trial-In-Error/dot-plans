ca64f2b2e1606a60599158533bce8c0e5d5a2565
Ashton Eby <ashton.eby@oracle.com>
2023-07-02 19:15:08 -0700

    first draft of left page (mon-wed)

--- compiler.mjs
+++ compiler.mjs
@@ -2,10 +2,13 @@ import * as fs from 'node:fs'
 import * as path from 'node:path'
 import Handlebars from 'handlebars'
 import livereload from 'livereload'
+import handlebarsHelperRepeat from 'handlebars-helper-repeat'
 
 const server = livereload.createServer()
 const lastTemplateValues = {}
 const paths = []
+
+Handlebars.registerHelper('repeat', handlebarsHelperRepeat)
 
 function registerFile(templatePath) {
   lastTemplateValues[templatePath] = ''
@@ -32,17 +35,19 @@ function startHotReload() {
 function compileTemplate(eventType, templatePath) {
   templatePath = path.normalize('./templates/' + path.basename(templatePath, path.extname(templatePath)) + '.handlebars')
   let template = String(fs.readFileSync(templatePath))
+  let times = ["6:30", "7:00", "7:30", "8:00", "7:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30"]
+  let days = ["Monday", "Tuesday", "Wednesday"]
+
   // TODO: this fires twice per save?
   if (eventType === 'change') {
     let css
     console.log('Updated %s', templatePath)
     try {
       css = String(fs.readFileSync(buildCSSPath(templatePath)))
-      console.log('CSS exists', css)
     } catch (e) {}
     lastTemplateValues[templatePath] = template
     const compiled = Handlebars.compile(template)
-    const context = { doesWhat: "compiles", css }
+    const context = { times, days, css }
     const string = compiled(context)
     fs.writeFileSync(buildHTMLPath(templatePath), string)
   }
