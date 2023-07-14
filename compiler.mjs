89b705e0b26eb1d0a7c5fe5406804c1034223f7b
Ashton Eby <ashton.eby@oracle.com>
2023-07-13 17:11:56 -0700

    fix bug with 7:30 am being listed twice

--- compiler.mjs
+++ compiler.mjs
@@ -37,7 +37,7 @@ function startHotReload() {
 function compileTemplate(eventType, templatePath) {
   templatePath = path.normalize('./templates/' + path.basename(templatePath, path.extname(templatePath)) + '.handlebars')
   let template = String(fs.readFileSync(templatePath))
-  let times = ["6:30", "7:00", "7:30", "8:00", "7:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30"]
+  let times = ["6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00", "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30"]
   let days = ["Monday", "Tuesday", "Wednesday"]
 
   // TODO: this fires twice per save?
